197d322dcb5b111619b8512765a198327e72914c
GaspachoGit <gaston2.0409@gmail.com>
2024-06-20 23:44:23 -0300

    Add file: ej1

---
+++ Ej1.py
@@ -0,0 +1,8 @@
+#Solicitar al usuario que ingrese su nombre y mostrar por pantalla.
+
+# Solicitar al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Mostrar el nombre ingresado por el usuario
+print(f"Hola, {nombre}!")
+
